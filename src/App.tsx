60833e409775371f1e5890862e0e699408398983
mr-burhanuddin <bjpresswala@gmail.com>
2024-09-03 14:08:19 +0530

    Try

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,72 @@
+
+/*
+
+**  File Name:        App.tsx
+
+**
+
+**  Description:      It is a test
+
+**
+
+**    Author:               Burhanuddin
+
+**  Creation Date:  2024-09-03
+
+**
+
+******************************************************************************
+
+**                                                                            **
+
+**                          COPYRIGHT                                         **
+
+**                                                                            **
+
+** (C) Copyright 2024                                                         **
+
+** Cygnus Compliance Consulting, Inc.                                        **
+
+**                                                                            **
+
+** This software is furnished under a license for use only on a single       **
+
+** computer system and may be copied only with the inclusion of the  above    **
+
+** copyright notice. This software or any other copies thereof, may not be **
+
+** provided or otherwise made available to any other person  except for use     **
+
+** on such system and to one who agrees to these license terms. title and   **
+
+** ownership of the software shall at all times remain in                   **
+
+** Cygnus Compliance Consulting, Inc.                                       **
+
+**                                                                            **
+
+** The information in this software is subject to change without notice and **
+
+** should not be construed as a commitment by                                **
+
+** Cygnus Compliance Consulting, Inc.                                       **
+
+******************************************************************************
+
+                                                     
+
+******************************************************************************
+
+                              Maintenance History                         
+
+-------------|----------|----------------------------------------------------
+
+    Date     |  Person  |  Description of Modification                   
+
+-------------|----------|----------------------------------------------------
+
+*/
+
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -25,9 +94,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
