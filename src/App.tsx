8591da3617b057d6eb20e1940af9bde26c5bd4bb
mr-burhanuddin <bjpresswala@gmail.com>
2024-09-03 19:51:00 +0530

    Revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,3 @@
-
-/*
-
-**  File Name:        App.tsx
-
-**
-
-**  Description:      Hello World
-
-**
-
-**    Author:               Burhanuddin
-
-**  Creation Date:  2024-09-03
-
-**
-
-******************************************************************************
-
-**                                                                            **
-
-**                          COPYRIGHT                                         **
-
-**                                                                            **
-
-** (C) Copyright 2024                                                         **
-
-** Cygnus Compliance Consulting, Inc.                                        **
-
-**                                                                            **
-
-** This software is furnished under a license for use only on a single       **
-
-** computer system and may be copied only with the inclusion of the  above    **
-
-** copyright notice. This software or any other copies thereof, may not be **
-
-** provided or otherwise made available to any other person  except for use     **
-
-** on such system and to one who agrees to these license terms. title and   **
-
-** ownership of the software shall at all times remain in                   **
-
-** Cygnus Compliance Consulting, Inc.                                       **
-
-**                                                                            **
-
-** The information in this software is subject to change without notice and **
-
-** should not be construed as a commitment by                                **
-
-** Cygnus Compliance Consulting, Inc.                                       **
-
-******************************************************************************
-
-                                                     
-
-******************************************************************************
-
-                              Maintenance History                         
-
--------------|----------|----------------------------------------------------
-
-    Date     |  Person  |  Description of Modification                   
-
--------------|----------|----------------------------------------------------
-
-*/
-
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -91,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR World
         </p>
       </div>
     </>
